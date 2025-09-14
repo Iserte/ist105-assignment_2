f659f060402f276a9cf803a30a37fd039bb4b4c2
Iserte <iserte@bisonic.io>
2025-09-14 15:03:45 -0700

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,13 +1,17 @@
 import cgi
 import math
 
-print("Assignment #2\n")
-print("Gustavo Iserte Bonfim - CT1010953\n")
+print("Assignment #2", end="\n")
+print("Gustavo Iserte Bonfim - CT1010953", end="\n")
 
 form = cgi.FieldStorage()
 a = float(form.getvalue("a", 1))
 b = float(form.getvalue("b", 1))
 c = float(form.getvalue("c", 1))
+
+print("A = ", a)
+print("B = ", b)
+print("C = ", c)
 
 c_cubed = c ** 3
 sqrt_cubed = c_cubed ** 0.5
