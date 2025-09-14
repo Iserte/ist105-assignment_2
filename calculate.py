e8dcbc6ee1d309615987c071a9b747ba596ee1e2
Iserte <iserte@bisonic.io>
2025-09-14 15:12:49 -0700

    Format html output

--- calculate.py
+++ calculate.py
@@ -1,16 +1,9 @@
 import sys
 import math
 
-print("Assignment #2", end="\n")
-print("Gustavo Iserte Bonfim - CT1010953", end="\n")
-
 a = float(sys.argv[1])
 b = float(sys.argv[2])
 c = float(sys.argv[3])
-
-print("A = ", a)
-print("B = ", b)
-print("C = ", c)
 
 c_cubed = c ** 3
 sqrt_cubed = c_cubed ** 0.5
@@ -18,4 +11,17 @@ division = sqrt_cubed / a
 multiplied = division * 10
 result = multiplied + b
 
-print(f"<html><body><h2>Result: {result:.2f}</h2></body></html>")
+print("Content-type: text/html\n")
+print(f"""
+<html>
+  <head><title>Assignment #2</title></head>
+  <body>
+    <h1>Assignment #2</h1>
+    <p><strong>Gustavo Iserte Bonfim - CT1010953</strong></p>
+    <p>A = {a}</p>
+    <p>B = {b}</p>
+    <p>C = {c}</p>
+    <h2>Result: {result:.2f}</h2>
+  </body>
+</html>
+""")
